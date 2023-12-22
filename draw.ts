cc6b495d4208b64d367795d1698bd00ff0afd7c7
Dolorre <75703911+Dolorre@users.noreply.github.com>
2023-12-22 15:23:42 -0600

    Changes 2

--- draw.ts
+++ draw.ts
@@ -1,5 +1,4 @@
 namespace fancyText {
-    const rainbow = img`245768ca`
 
     export function drawFontText(left: number, top: number, text: string, lines: Line[], defaultColor: number, defaultFont: fancyText.BaseFont, length: number) {
         let currentLeft = left;
@@ -7,8 +6,8 @@ namespace fancyText {
         for (const line of lines) {
             currentLeft = left;
             for (const span of line.spans) {
-                const font = getFontForSpan(span.flags) || defaultFont;
-                const color = getColorForSpan(span.flags) || defaultColor;
+                const font = defaultFont;
+                const color = defaultColor;
 
                 if (font.lineHeight === line.height) {
                     drawFontSpan(currentLeft, top, text.substr(span.offset, span.length), font, color, span.flags, left, length);
@@ -33,49 +32,6 @@ namespace fancyText {
         if (flags & Tag.Color01) {
             return 1
         }
-        if (flags & Tag.Color02) {
-            return 2
-        }
-        if (flags & Tag.Color03) {
-            return 3
-        }
-        if (flags & Tag.Color04) {
-            return 4
-        }
-        if (flags & Tag.Color05) {
-            return 5
-        }
-        if (flags & Tag.Color06) {
-            return 6
-        }
-        if (flags & Tag.Color07) {
-            return 7
-        }
-        if (flags & Tag.Color08) {
-            return 8
-        }
-        if (flags & Tag.Color09) {
-            return 9
-        }
-        if (flags & Tag.Color10) {
-            return 10
-        }
-        if (flags & Tag.Color11) {
-            return 11
-        }
-        if (flags & Tag.Color12) {
-            return 12
-        }
-        if (flags & Tag.Color13) {
-            return 13
-        }
-        if (flags & Tag.Color14) {
-            return 14
-        }
-        if (flags & Tag.Color15) {
-            return 15
-        }
-
         return 0;
     }
 
@@ -102,9 +58,7 @@ namespace fancyText {
                 if (flags & Tag.Wavy) {
                     y += Math.sin(((x - absoluteLeft) / 10) + tick) * 2;
                 }
-                if (flags & Tag.Rainbow) {
-                    color = rainbow.getPixel((Math.idiv(x - absoluteLeft, width) + tick) % rainbow.width, 0);
-                }
+
                 const char = text.charAt(i);
                 const code = text.charCodeAt(i);
 
@@ -378,8 +332,8 @@ namespace fancyText {
         for (const line of lines) {
             currentLeft = left;
             for (const span of line.spans) {
-                const font = getFontForSpan(span.flags) || defaultFont;
-                const color = getColorForSpan(span.flags) || defaultColor;
+                const font = defaultFont;
+                const color = defaultColor;
 
                 if (font.lineHeight === line.height) {
                     drawFontSpanInBox(currentLeft, top, boxLeft, boxTop, boxRight, boxBottom, text.substr(span.offset, span.length), font, color, span.flags, left, length);
@@ -422,9 +376,7 @@ namespace fancyText {
                 if (flags & Tag.Wavy) {
                     y += Math.sin(((x - absoluteLeft) / 10) + tick) * 2;
                 }
-                if (flags & Tag.Rainbow) {
-                    color = rainbow.getPixel((Math.idiv(x - absoluteLeft, width) + tick) % rainbow.width, 0);
-                }
+
                 const char = text.charAt(i);
                 const code = text.charCodeAt(i);
 
--- parse.ts
+++ parse.ts
@@ -1,21 +1,6 @@
 namespace fancyText {
     export enum Tag {
         Color01 = 1 << 0,
-        Color02 = 1 << 1,
-        Color03 = 1 << 2,
-        Color04 = 1 << 3,
-        Color05 = 1 << 4,
-        Color06 = 1 << 5,
-        Color07 = 1 << 6,
-        Color08 = 1 << 7,
-        Color09 = 1 << 8,
-        Color10 = 1 << 9,
-        Color11 = 1 << 10,
-        Color12 = 1 << 11,
-        Color13 = 1 << 12,
-        Color14 = 1 << 13,
-        Color15 = 1 << 14,
-        Font5 = 1 << 15,
         Font8 = 1 << 16,
         Font12 = 1 << 17,
         Shaky = 1 << 18,
@@ -167,86 +152,11 @@ namespace fancyText {
 
     function getTagFlag(name: string) {
         switch (name) {
-            case "c1":
-            case "color1":
             case "white":
-                return Tag.Color01;
-            case "c2":
-            case "color2":
-            case "red":
-                return Tag.Color02;
-            case "c3":
-            case "color3":
-            case "pink":
-                return Tag.Color03;
-            case "c4":
-            case "color4":
-            case "orange":
-                return Tag.Color04;
-            case "c5":
-            case "color5":
-            case "yellow":
-                return Tag.Color05;
-            case "c6":
-            case "color6":
-            case "teal":
-                return Tag.Color06;
-            case "c7":
-            case "color7":
-            case "green":
-                return Tag.Color07;
-            case "c8":
-            case "color8":
-            case "blue":
-                return Tag.Color08;
-            case "c9":
-            case "color9":
-            case "cyan":
-                return Tag.Color09;
-            case "c10":
-            case "color10":
-            case "purple":
-                return Tag.Color10;
-            case "c11":
-            case "color11":
-            case "lightpurple":
-                return Tag.Color11;
-            case "c12":
-            case "color12":
-            case "darkpurple":
-                return Tag.Color12;
-            case "c13":
-            case "color13":
-            case "tan":
-                return Tag.Color13;
-            case "c14":
-            case "color14":
-            case "brown":
-                return Tag.Color14;
-            case "c15":
-            case "color15":
-            case "black":
-                return Tag.Color15;
-            case "small":
-            case "font5":
-                return Tag.Font5;
+                return Tag.Color01;       
             case "medium":
             case "font8":
                 return Tag.Font8;
-            case "big":
-            case "font12":
-                return Tag.Font12;
-            case "shake":
-            case "shaky":
-                return Tag.Shaky;
-            case "wave":
-            case "wavy":
-                return Tag.Wavy;
-            case "blink":
-            case "blinking":
-                return Tag.Blinking;
-            case "rainbow":
-                return Tag.Rainbow;
             case "veryslow":
                 return Tag.VerySlow;
             case "slow":
@@ -261,7 +171,7 @@ namespace fancyText {
     }
 
     export function getFontForSpan(flags: number) {
-        if (flags & Tag.Font5) {
+        if (flags & Tag.Font8) {
             return new fancyText.LegacyFont(image.font5);
         }
         else if (flags & Tag.Font8) {
