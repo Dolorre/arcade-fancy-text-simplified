b7e2e0617fbdda5d6b50b72e622721b69aca7c76
Dolorre <75703911+Dolorre@users.noreply.github.com>
2023-12-22 09:35:54 -0600

    removing unused code

--- draw.ts
+++ draw.ts
@@ -33,92 +33,12 @@ namespace fancyText {
         if (flags & Tag.Color01) {
             return 1
         }
-        if (flags & Tag.Color02) {
-            return 2
-        }
-        if (flags & Tag.Color03) {
-            return 3
-        }
-        if (flags & Tag.Color04) {
-            return 4
-        }
-        if (flags & Tag.Color05) {
-            return 5
-        }
-        if (flags & Tag.Color06) {
-            return 6
-        }
-        if (flags & Tag.Color07) {
-            return 7
-        }
-        if (flags & Tag.Color08) {
-            return 8
-        }
-        if (flags & Tag.Color09) {
-            return 9
-        }
-        if (flags & Tag.Color10) {
-            return 10
-        }
-        if (flags & Tag.Color11) {
-            return 11
-        }
-        if (flags & Tag.Color12) {
-            return 12
-        }
-        if (flags & Tag.Color13) {
-            return 13
-        }
-        if (flags & Tag.Color14) {
-            return 14
-        }
-        if (flags & Tag.Color15) {
-            return 15
-        }
-
+        
         return 0;
     }
 
     function drawFontSpan(left: number, top: number, text: string, font: fancyText.BaseFont, color: number, flags: number, absoluteLeft: number, length: number) {
-        if (flags & Tag.Blinking) {
-            if (Math.idiv(game.runtime(), 250) % 2 === 0) {
-                return;
-            }
-        }
-        if (flags & (Tag.Wavy | Tag.Shaky | Tag.Rainbow)) {
-            const width = getTextWidth(font, text) / text.length;
-
-            const tick = Math.idiv(game.runtime(), 100);
-            let x = left;
-
-            for (let i = 0; i < text.length; i++) {
-                if (i >= length) return;
-
-                let y = top;
-                if (flags & Tag.Shaky) {
-                    x += randint(-1, 1);
-                    y += randint(-1, 1);
-                }
-                if (flags & Tag.Wavy) {
-                    y += Math.sin(((x - absoluteLeft) / 10) + tick) * 2;
-                }
-                if (flags & Tag.Rainbow) {
-                    color = rainbow.getPixel((Math.idiv(x - absoluteLeft, width) + tick) % rainbow.width, 0);
-                }
-                const char = text.charAt(i);
-                const code = text.charCodeAt(i);
-
-                printText(screen, font, char, x, y, color);
-
-                if (char == " " || !font.isInFont(code)) {
-                    x += font.wordSpacing;
-                }
-                else {
-                    x += font.charWidth(code) + font.letterSpacing;
-                }
-            }
-        }
-        else if (length < text.length) {
+        if (length < text.length) {
             printText(screen, font, text.substr(0, length), left, top, color);
         }
         else {
@@ -400,45 +320,7 @@ namespace fancyText {
     }
 
     function drawFontSpanInBox(left: number, top: number, boxLeft: number, boxTop: number, boxRight: number, boxBottom: number, text: string, font: fancyText.BaseFont, color: number, flags: number, absoluteLeft: number, length: number) {
-        if (flags & Tag.Blinking) {
-            if (Math.idiv(game.runtime(), 250) % 2 === 0) {
-                return;
-            }
-        }
-        if (flags & (Tag.Wavy | Tag.Shaky | Tag.Rainbow)) {
-            const width = getTextWidth(font, text) / text.length;
-
-            const tick = Math.idiv(game.runtime(), 100);
-            let x = left;
-
-            for (let i = 0; i < text.length; i++) {
-                if (i >= length) return;
-
-                let y = top;
-                if (flags & Tag.Shaky) {
-                    x += randint(-1, 1);
-                    y += randint(-1, 1);
-                }
-                if (flags & Tag.Wavy) {
-                    y += Math.sin(((x - absoluteLeft) / 10) + tick) * 2;
-                }
-                if (flags & Tag.Rainbow) {
-                    color = rainbow.getPixel((Math.idiv(x - absoluteLeft, width) + tick) % rainbow.width, 0);
-                }
-                const char = text.charAt(i);
-                const code = text.charCodeAt(i);
-
-                printTextInBox(screen, font, char, x, y, boxLeft, boxTop, boxRight, boxBottom, color);
-
-                if (char == " " || !font.isInFont(code)) {
-                    x += font.wordSpacing;
-                }
-                else {
-                    x += font.charWidth(code) + font.letterSpacing;
-                }
-            }
-        }
-        else if (length < text.length) {
+        if (length < text.length) {
             printTextInBox(screen, font, text.substr(0, length), left, top, boxLeft, boxTop, boxRight, boxBottom, color);
         }
         else {
